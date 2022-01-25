604c186e65ad72fc7bf7a69fcb5effff28336a46
danny0x4 <dannyrestu134@gmail.com>
2022-01-25 12:42:05 +0800

    breakfile

---
+++ break.py
@@ -0,0 +1,31 @@
+# Break
+
+#angka = 0
+
+# while angka < 5:
+#     angka += 1
+#     print(f"angka sekarang -> {angka}")
+
+#     if angka == 3:
+#         print("Nice!")
+#         break
+
+#     print("Helow")
+
+# print("Selesai")
+
+
+data_int = int(input("Hitung sampai = "))
+angka = 0
+
+while True:
+    angka += 1
+    print(f"count = {angka}")
+
+    if angka == data_int:
+        print("Nice!")
+        break
+
+    print("Helow")
+
+print("Selesai")
